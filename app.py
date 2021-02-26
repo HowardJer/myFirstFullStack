14eb8f042b71ce2250a2a76790569d499f89da52
Howard <howardjer@gmail.com>
2021-02-25 21:42:06 -0700

    backend seems to work

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def connect():
         database = dbcreds.database 
     )
 
-@app.route("/blog_list",methods=["GET", "POST", "PATCH", "DELETE"])
+@app.route("/blog_list",methods=["GET", "POST", "PATCH", "DELETE"])  
 def blog():
     if request.method == "GET":
         conn = None
@@ -51,7 +51,7 @@ def blog():
                     mimetype="application/json",
                     status=200)
             else: 
-                return Response("Failed", mimetype="html/text", status=200) #400
+                return Response("Failed", mimetype="html/text", status=400) #400           
     if request.method == "POST":
         content = request.json.get("content")
         conn = None
@@ -88,11 +88,11 @@ def blog():
             else: 
                 return Response(
                     "Failed", 
-                    mimetype="html/text",     status=200 #400
+                    mimetype="html/text", status=200 #400
                 )            
     if request.method == "PATCH":
         content = request.json.get("content")
-        blog_id =request.json.get("id")
+        blog_id = request.json.get("id")
         conn = None
         cursor = None
         try:
@@ -101,7 +101,8 @@ def blog():
             cursor.execute("UPDATE blog_list SET  content =? WHERE id=?",
                            [content, blog_id])
             conn.commit()
-            blog_id = cursor.lastrowid
+            rowcount = cursor.rowcount
+            
         except Exception as e:
             print(e)
         finally: 
@@ -110,14 +111,11 @@ def blog():
                 conn.close() 
             if(cursor != None):
                 cursor.close()
-            if(blog_id != None ):
-
-
+            if(rowcount == 1):
                 blog ={
                     "id":blog_id,
                     "content": content
                 }              
-
                 return Response(json.dumps(blog, default=str),
                                 mimetype="application/json",status=200)  #201
             else: 
@@ -132,8 +130,8 @@ def blog():
             cursor.execute("DELETE FROM blog_list WHERE id=?",[blog_id])
             rows = cursor.rowcount
             conn.commit()
-            blog_id = cursor.lastrowid
-        except Exception as e:
+            rowcount = cursor.rowcount
+            except Exception as e:
             print(e)
         finally: 
             if(conn != None):
@@ -141,7 +139,7 @@ def blog():
                 conn.close() 
             if(cursor != None):
                 cursor.close()
-            if(rows == 1): 
+            if(rowcount == 1): 
 
                 return Response("Success",mimetype="html/text",status=200)  #201
             else: 
